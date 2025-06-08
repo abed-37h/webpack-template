e227c930303a4e83b1e25430bfe637bbf5aa8e60
abed-37h <79279228+abed-37h@users.noreply.github.com>
2025-06-08 04:21:21 +0300

    refactor: Add JSDoc and private fields to Footer
    - Added JSDoc comments for the class, constructor, and methods
    - Converted author and githubUrl to private fields

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,21 +3,46 @@ import { Component } from "./component";
 import './footer.css';
 import GithubLogo from '../assets/icons/github.svg';
 
+/**
+ * A UI component representing the footer section of the application.
+ * Displays author's name and a GitHub link.
+ * 
+ * @extends Component
+ */
 export class Footer extends Component {
+    /**
+     * @type {string}
+     */
+    #author;
+    /**
+     * @type {string}
+     */
+    #githubUrl;
+
+    /**
+     * Class constructor
+     * @param {String} author 
+     * @param {String} githubUrl 
+     */
     constructor(author, githubUrl) {
         super();
-        this.author = author;
-        this.githubUrl = githubUrl;
+        this.#author = author;
+        this.#githubUrl = githubUrl;
     }
 
+    /**
+     * Renders the component
+     * @returns {DomElement}
+     * @override
+     */
     render() {
         const footer = new DomElement('footer')
             .append(
                 new DomElement('p')
                     .setClass('copyright-text')
-                    .setText(`\u00A9 ${new Date().getFullYear()} ${this.author}`),
+                    .setText(`\u00A9 ${new Date().getFullYear()} ${this.#author}`),
                 new DomElement('a')
-                    .setAttributes({ href: this.githubUrl, target: '_blank', rel: 'noopener' })
+                    .setAttributes({ href: this.#githubUrl, target: '_blank', rel: 'noopener' })
                     .append(
                         new DomElement('img')
                             .setClass('icon github')
